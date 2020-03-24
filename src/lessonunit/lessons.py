16ec568eca7164491d9f3adbb383a9f8edd26059
Adam Spann <adam_spann@hotmail.com>
2020-03-24 20:07:42 +0900

    Initial Lessons object definition
    This is just a basic outline

--- src/lessonunit/lessons.py
+++ src/lessonunit/lessons.py
@@ -1,6 +1,9 @@
 class Lessons(object):
     """docstring for Lessons."""
 
-    def __init__(self, arg):
-        super(Lessons, self).__init__()
-        self.arg = arg
+    def __init__(self,date=None,month=None,year=None):
+        self._date = date
+        self._month = month
+        self._year = year
+        self.classes = []
+        
