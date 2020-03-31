6bd0edfa7cae6dc85bb2e68d0b3d1d58e31c6a2a
Adam Spann <adam_spann@hotmail.com>
2020-03-31 16:14:13 +0900

    Renamed unit_exceptions.py to just exceptions.py

---
+++ src/units/exceptions.py
@@ -0,0 +1,17 @@
+class UnitExceptions(Exception):
+    """Base Class exceptions for Unit."""
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return "{}".format(self.msg)
+
+class UnitCountValueInvalidError(UnitExceptions):
+    """UnitCountValueInvalid."""
+    def __init__(self, msg="UnitCountValueInvalidError occured"):
+        super().__init__(msg)
+
+class UnitTimeCountValueMismatchError(UnitExceptions):
+    """docstring for UnitTimeCountValueMismatchError.UnitException"""
+    def __init__(self, msg="Time difference does not match count"):
+        super().__init__(msg)
