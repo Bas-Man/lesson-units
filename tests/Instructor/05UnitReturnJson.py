15674e6fb904fcfaa8952241edb00eccad929669
Adam Spann <Bas-Man@users.noreply.github.com>
2020-04-09 10:11:28 +0900

    This needs to be re-examined. unable to call at this time.
    May need to look at doing implementation?

---
+++ tests/Instructor/05UnitReturnJson.py
@@ -0,0 +1,21 @@
+import unittest
+
+from context import exceptions as UnitExcpt
+from context import instructor
+
+class UnitTimesTests(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = instructor.UnitInstructor()
+
+    def testObjectJsonStr(self):
+        self.obj.createUnit(startTime="07:00", endTime="07:40")
+        lines = self.obj.json
+        self.assertTrue('"_startTime": "07:00"' in lines)
+
+    def testObjectJsonStr(self):
+        self.obj.createUnit(startTime="07:00", endTime="07:40",
+                            comment="Akasaka", location="Akasaka")
+        lines = self.obj.json
+        self.assertTrue('"_comment": "Akasaka"' in lines)
+        self.assertTrue('"_location": "Akasaka"' in lines)
