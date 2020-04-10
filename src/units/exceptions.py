5afbaa46fd0a0d1000aa74bd6fa97134f7a3ea83
Adam Spann <Bas-Man@users.noreply.github.com>
2020-04-10 15:24:13 +0900

    Added base unit.Instructor Exception class Derived exception for when _type is None and _bonus is True. This is not allowed.

--- src/units/exceptions.py
+++ src/units/exceptions.py
@@ -19,3 +19,16 @@ class UnitInvalidStartTimeError(UnitExceptions):
 class UnitInvalidEndTimeError(UnitExceptions):
     def __init__(self, msg="Invalid End Time provided."):
         super().__init__(msg)
+
+###
+### Exception classes for unit.Instructor begin from here
+###
+
+class InstructorError(UnitExceptions):
+    """Base Class exception for unit.Instructor"""
+    def __init__(self,msg="Error in unit.Instructor"):
+        super().__init__(msg)
+
+class TypeBonusInstructorError(InstructorError):
+    def __init__(self, msg="Invalid: _type is None but _bonus is True."):
+        super().__init__(msg)
