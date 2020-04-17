243c2a42b08a9d1b80cdc846dd4643ee2f2029ec
Adam Spann <Bas-Man@users.noreply.github.com>
2020-04-17 23:31:23 +0900

    Added docstring to __init__ Just a bit of information about this package and its purpose.

--- src/units/__init__.py
+++ src/units/__init__.py
@@ -1,1 +1,9 @@
+"""
+This package contains modules for creating and managing Unit objects.
+It deals with Student and Instructor Objects.
+unit.Student inherits from unit.Unit.
+unit.Instructor inherits from unit.Student
 
+unit.dailyunits allows one to create a list of Units that belong to a Student
+or Instructor object. Effectively creating a daily schedule.
+"""
