90d69aca27b035bae66fc8601e0747e91431a72b
Adam Spann <Bas-Man@users.noreply.github.com>
2020-04-27 16:12:13 +0900

    updated setup.py Added some requirements. These are still likely to change.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages
 from distutils.core import setup
 
+requirements = []
+
 setup(
     name='units',
     version='0.1.0',
@@ -10,5 +12,21 @@ setup(
     package_dir={'': 'src'},
     description='Module for handling Units for Lessons',
     long_description=open('README.md').read(),
-    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7'
+    ],
+    python_requires='>=3',
+    install_requires=requirements,
+    extra_requires={
+        'dev': [
+            'pytest',
+            'unittest',
+            'coverage',
+            'pytest-cov'
+        ],
+    }
 )
